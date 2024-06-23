ab5dcf27a10cd97f42c66d1cd69d03d509ec9759
Angela <nyabutiangella@gmail.com>
2024-06-23 14:07:30 +0300

    Added the invoices file and the data grid tables to it

--- admin-panel/src/scenes/invoices/index.jsx
+++ admin-panel/src/scenes/invoices/index.jsx
@@ -1,25 +1,22 @@
-import { Box } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box, Typography, useTheme} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import {mockDataContacts} from "../../data/mockData";
+import {mockDataInvoices} from "../../data/mockData";
 import Header from "../../components/Header";
-import {useTheme} from "@mui/material";
 
 
 
-const Contacts = () =>{
+const Invoices = () =>{
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const columns = [
-        {field: "id", headerName: "ID", flex: 0.5},
-        {field: "registrarId", headerName: "Registrar ID"},
+        {field: "id", headerName: "ID"},
         {field: "name", headerName: "Name", flex: 1, cellClassName: "name-column--cell"},
-        {field: "age", headerName: "Age", type:"number", headerAlign: "left", align:"left"},
         {field: "phone", headerName: "Phone Number", flex:1},
-        {field: "email", headerName: "Email", flex: 1},
-        {field: "address", headerName: "Address", flex: 1},
-        {field: "city", headerName: "City", flex: 1},
-        {field: "zipCode", headerName: "ZipCode", flex: 1},
+        {field: "email", headerName: "Email", flex: 1, renderCell: (params) => (<Typography color={colors.greenAccent[500]}>${params.row.cost}</Typography>)},
+        {field: "cost", headerName: "Cost", flex: 1},
+        {field: "date", headerName: "Date", flex: 1},
+        
        
         
 
@@ -27,7 +24,7 @@ const Contacts = () =>{
 
     return(
         <Box m="20px" >
-            <Header title="CONTACTS" subtitle="List of Contacts for Future Reference"/>
+            <Header title="INVOICES" subtitle="List of invoice Balances"/>
             <Box m="40px 0 0 0" 
             sx={{
                 "& .MuiDataGrid-root" : {
@@ -50,20 +47,21 @@ const Contacts = () =>{
                     borderTop: "none",
                     backgroundColor: colors.blueAccent[700],
                 },
-                "& .MuiDataGrid-toolbarContainer .MuiButton-text" : {
-                    color: `${colors.grey[100]} !important`,
-                },
+                "& .MuiCheckbox-root" : {
+                    color: `${colors.greenAccent[200]} !important`
+                }
             }}
             
             >
                 <DataGrid
-                    rows={mockDataContacts}
+                    checkboxSelection
+                    rows={mockDataInvoices}
                     columns={columns}  
-                    components={{ Toolbar: GridToolbar}}              
+                                 
                 />
             </Box>
         </Box>
     )
 }
 
-export default Contacts;
+export default Invoices;
