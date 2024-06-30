b2c0ab720e43cf34b9d0e37807875ead2e291dcc
Angela <nyabutiangella@gmail.com>
2024-06-30 11:30:54 +0300

    Added the BarChart

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -7,7 +7,7 @@ import  Dashboard  from "./scenes/dashboard";
 import  Team  from "./scenes/team";
 import  Invoices  from "./scenes/invoices";
 import  Contacts  from "./scenes/contacts";
-//import  Bar  from "./scenes/bar";
+import  Bar  from "./scenes/bar";
 import  Form  from "./scenes/form";
 //import  Line  from "./scenes/line";
 //import  Pie  from "./scenes/pie";
@@ -32,8 +32,8 @@ function App() {
               <Route path="/invoices" element={<Invoices/>} />     
               <Route path="/form" element={<Form/>} />   
               <Route path="/faq" element={<FAQ/>} />      
-              {/*<Route path="/bar" element={<Bar/>} />              
-              <Route path="/line" element={<Line/>} />
+              <Route path="/bar" element={<Bar/>} />              
+              {/*<Route path="/line" element={<Line/>} />
               <Route path="/pie" element={<Pie/>} />
               
               <Route path="/geography" element={<Geography/>} />*/}
