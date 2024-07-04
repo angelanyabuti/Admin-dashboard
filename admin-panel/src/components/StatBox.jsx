d7b86cee9cf316f90aae4184e5fa5be72e2fbd8a
Angela <nyabutiangella@gmail.com>
2024-07-04 10:25:14 +0300

    Added the first row on the dashboard displaying the progress circles

--- admin-panel/src/components/StatBox.jsx
+++ admin-panel/src/components/StatBox.jsx
@@ -18,17 +18,18 @@ const StatBox = ({ title, subtitle, icon, progress, increase}) => {
                 <Box>
                     <ProgressCircle progress={progress} />
                 </Box>
+                </Box>    
                 <Box display="flex" justifyContent="space-between">
                 <Typography variant="h5"  sx={{color:colors.greenAccent[500]}}>
                         {subtitle}
                 </Typography>
                 <Typography variant="h5" fontStyle="italic" sx={{color:colors.greenAccent[600]}}>
-                        {title}
+                        {increase}
                     </Typography>
 
                 </Box>
 
-            </Box>
+            
         </Box>
     )
 }
